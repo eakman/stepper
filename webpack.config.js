4f320695c097b15b52adb460b93d3897265973e7
Eitan <eitanakman@gmail.com>
2016-12-18 01:01:58 -0500

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  entry: "./lib/first_tone.js",
+  output: {
+  	filename: "./lib/bundle.js"
+  },
+  devtool: 'source-map',
+  resolve: {
+      root: __dirname,
+      modulesDirectories : ["./node_modules/tone/build/Tone.js/"],
+  }
+};
